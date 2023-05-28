0f26f7a4a853fd88d22464b5de581dd0f00a58a9
dittyAvramovich <67556022+dittyAvramovich@users.noreply.github.com>
2023-05-28 12:05:43 +0300

    faljor

---
+++ FalkorTest/Classes/Hospital.cs
@@ -0,0 +1,130 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace FalkorTest.Classes
+{
+    public class Hospital
+    {
+        public static List<string> GetFloors1()
+        {
+            List<string> floors = new List<string>();
+            var custId=1;
+            using (SqlConnection conn = new SqlConnection("Server=DESKTOP-E68S884\\SQLEXPRESS;DataBase=Northwind;Integrated Security=SSPI"))
+            {
+                conn.Open();
+
+                // 1.  create a command object identifying the stored procedure
+                SqlCommand cmd = new SqlCommand("CustOrderHist", conn);
+
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // 3. add parameter to command, which will be passed to the stored procedure
+                cmd.Parameters.Add(new SqlParameter("@CustomerID", custId));
+
+                // execute the command
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    // iterate through results, printing each to console
+                    while (rdr.Read())
+                    {
+                        floors.Add(rdr.ToString());
+                        Console.WriteLine("Product: {0,-35} Total: {1,2}", rdr["ProductName"], rdr["Total"]);
+                    }
+                }
+            }
+
+            return new List<string>();
+        
+    }
+
+        public Object getData()
+        {
+            var custId = 1;
+
+            using (SqlConnection conn = new SqlConnection("Server=(local);DataBase=Northwind;Integrated Security=SSPI"))
+            {
+                conn.Open();
+
+                // 1.  create a command object identifying the stored procedure
+                SqlCommand cmd = new SqlCommand("getFloors", conn);
+
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // 3. add parameter to command, which will be passed to the stored procedure
+                cmd.Parameters.Add(new SqlParameter("@CustomerID", custId));
+
+                // execute the command
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    // iterate through results, printing each to console
+                    while (rdr.Read())
+                    {
+                        
+                        Console.WriteLine("Product: {0,-35} Total: {1,2}", rdr["ProductName"], rdr["Total"]);
+                    }
+                }
+            }
+ 
+            return new Object();    
+        }
+        
+    }
+}
+
+
+
+
+
+//public static List<ContentSearch> Get(string searchValue, string TextFalse, string TextTrue)
+//{
+//    List<ContentSearch> contentSearchs = new List<ContentSearch>();
+//    try
+//    {
+//        string connectionString = ConfigurationUtil.ConnectionStrings("QFlowDB").ConnectionString;
+//        using (SqlConnection cn = new SqlConnection(connectionString))
+//        {
+//            using (var cm = new SqlCommand("[cqf].[ContentTextSearch]", cn) { CommandType = CommandType.StoredProcedure })
+//            {
+//                cn.Open();
+//                string UserName = HttpContext.Current.User.Identity.Name;
+//                cm.Parameters.Add(new SqlParameter("@UserName", UserName));
+//                cm.Parameters.Add(new SqlParameter("@SearchValue", searchValue));
+//                cm.Parameters.Add(new SqlParameter("@TextFalse", TextFalse));
+//                cm.Parameters.Add(new SqlParameter("@TextTrue", TextTrue));
+//                using (var dr = cm.ExecuteReader(CommandBehavior.SingleResult))
+//                {
+//                    while (dr.Read())
+//                    {
+//                        TemplateObjectType etype = (TemplateObjectType)Convert.ToInt32(dr["ObjectType"]);
+//                        contentSearchs.Add(new ContentSearch
+//                        {
+//                            Value = dr["Value"].ToString(),
+//                            LanguageCode = dr["LanguageCode"].ToString(),
+//                            ContentTemplateId = Convert.ToInt32(dr["ContentTemplateId"]),
+//                            ObjectId = Convert.ToInt32(dr["ObjectId"]),
+//                            ObjectType = etype.ToString(),
+//                            ObjectName = dr["ObjectName"].ToString(),
+//                            ParameterName = dr["ParameterName"].ToString(),
+//                            Description = dr["DescriptionObject"].ToString(),
+//                            IsContentItem = Convert.ToBoolean(dr["IsContentItem"]),
+//                            NumberPage = dr["PageNum"].ToString()
+
+//                        }); ;
+
+//                    }
+//                    dr.Close();
+//                }
+//                cn.Close();
+//            }
+//        }
+//    }
+//    catch (Exception ex)
+//    {
+//        EventLogEntry ev = new EventLogEntry(EventLogEntryType.Error, "ContentSearchAPI.DAL.ContentSearchRepository.Get", ex.ToString(), 0, 0);
+//        EventLog.Write(ref ev);
+//    }
+//    return contentSearchs;
+//}
---
+++ FalkorTest/JavaScript.js
@@ -0,0 +1,81 @@
+﻿
+    window.onload = function () {
+        alert("iiiiii");
+        const api_url =
+            "https://localhost:7051/getFloors";
+
+        // Defining async function
+        async function getapi(url) {
+
+            // Storing response
+            const response = await fetch(url);
+
+            // Storing data in form of JSON
+            var data = await response.json();
+            console.log(data);
+            if (response) {
+                hideloader();
+            }
+            show(data);
+        }
+        // Calling that async function
+        getapi(api_url);
+
+    };
+function favTutorial() {
+    var mylist = document.getElementById("myList");
+    document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;
+}
+function table() {
+    var x = document.getElementById("myTable");
+    x.deleteRow(0);
+}
+// api url
+function load() {
+    const api_url =
+        "https://localhost:7051/api/getFloors";
+
+    // Defining async function
+    async function getapi(url) {
+
+        // Storing response
+        const response = await fetch(url);
+
+        // Storing data in form of JSON
+        var data = await response.json();
+        console.log(data);
+        if (response) {
+            hideloader();
+        }
+        show(data);
+    }
+    // Calling that async function
+    getapi(api_url);
+}
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab =
+        `<tr>
+          <th>שעת זימון</th>
+          <th>כירורוגיה</th>
+          <th>מוס</th>
+          <th>עיניים</th>
+          <th>עפעפיים</th>
+         </tr>`;
+
+    // Loop to access all rows
+    for (let r of data.list) {
+        tab += `<tr>
+    <td>${r.name} </td>
+    <td>${r.office}</td>
+    <td>${r.position}</td>
+    <td>${r.salary}</td>         
+</tr>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("employees").innerHTML = tab;
+}
